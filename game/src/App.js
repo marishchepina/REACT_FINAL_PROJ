5ff2225bc4ddf04d381e108833cb51526ca0637e
Mari Shchepina <marishchepina@gmail.com>
2021-01-13 04:36:37 +0200

    App.js is functional componenet now

--- game/src/App.js
+++ game/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import AllWordsList from './data'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ShowWord from './components/ShowWord/ShowWord'
@@ -7,60 +7,52 @@ import Menu from './components/Menu/Menu'
 import Home from './components/Home/Home'
 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      menu: false,
-      key: 1,
-      allWords: AllWordsList,
-      activeWordSet: AllWordsList[0].wordSet
-    }
-  }
 
-  toggleMenuHandler = () => {
-    this.setState({
-      menu: !this.state.menu
-    })
+function App() {
+  const [menu, setMenu] = useState(false)
+  const [key, setKey] = useState(1)
+  const [activeWordSet, setActiveWordSet] = useState(AllWordsList[0].wordSet)
+
+
+  const toggleMenuHandler = () => {
+    setMenu(!menu)
   }
 
 
-  handleLinkChoise = (linkNuber) => {
-    this.setState({
-      menu: false,
-      key: Math.random(),
-      activeWordSet: AllWordsList[linkNuber - 1].wordSet
-    })
+  const handleLinkChoise = (linkNuber) => {
+    setMenu(false)
+    setKey(Math.random())
+    setActiveWordSet(AllWordsList[linkNuber - 1].wordSet)
   }
 
 
-  render() {
-    return (
-      <Router>
-        <div className='container'>
-          <MenuToggle
-            onToggle={this.toggleMenuHandler}
-            isOpen={this.state.menu}
-          />
-          <Menu
-            allWords={this.state.allWords}
-            isOpen={this.state.menu}
-            onClick={this.handleLinkChoise}
-          />
-          <Switch>
-            <Route exact path="/"><Home /></Route>
-            <Route>
-              <ShowWord
-                key={this.state.key}
-                wordSet={this.state.activeWordSet}
-              />
-            </Route>
-          </Switch>
-        </div>
-      </Router >
-    );
-  }
+
+  return (
+    <Router>
+      <div className='container'>
+        <MenuToggle
+          onToggle={toggleMenuHandler}
+          sisOpen={menu}
+        />
+        <Menu
+          allWords={AllWordsList}
+          isOpen={menu}
+          onClick={handleLinkChoise}
+        />
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route>
+            <ShowWord
+              key={key}
+              wordSet={activeWordSet}
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router >
+  )
 }
+
 
 
 export default App;
